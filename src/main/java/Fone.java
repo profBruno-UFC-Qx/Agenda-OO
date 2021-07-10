39fec98ba1b1cf22ff0215260364c4a484fee399
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-09 22:29:25 -0300

    adicionando classes

---
+++ src/main/java/Fone.java
@@ -0,0 +1,32 @@
+public class Fone {
+    
+    private String identificador;
+    private String numero;
+
+    public Fone(String identificador, String numero){
+        this.identificador = identificador;
+        this.numero = numero;
+    }
+
+    public boolean validarNumero(String numero){
+        String validos = "0123456789()-.";
+        for(int i=0;i<numero.length();i++){
+            if(validos.charAt(numero.charAt(i)) == -1)
+                return false;
+        }
+        return true;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + identificador + ":" + numero + "]"; 
+    }
+}
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
 
     public static void main(final String[] args) {
-        System.out.println("Hello world!");
+        
+        
     }
 }
